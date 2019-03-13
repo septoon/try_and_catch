1184a5a28ddb20892b161993c21646d01638ddb9
septoon <41443306+septoon@users.noreply.github.com>
2019-03-13 23:51:49 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,17 +1,4 @@
-// import { getNumber } from '../src/js/app';
-
-
-const getNumber = () => {
-  try {
-    const result = parseInt(number);
-    if (isNaN(result)) {
-      throw new SyntaxError('Error');
-    }
-    console.log(result);
-  } catch (e) {
-    console.error("It's not a number");
-  }
-};
+import { getNumber } from '../src/js/app';
 
 
 test('should be a number', () => {
@@ -24,16 +11,3 @@ test('throw', () => {
     isNaN(result);
   }).toThrow();
 });
-
-
-
-
-// export const getNumber = (a, b) => {
-//   return a + b;
-// }
-
-
-// test("add", () => {
-//   expect(2 + 2).toBe(4);
-
-// });
